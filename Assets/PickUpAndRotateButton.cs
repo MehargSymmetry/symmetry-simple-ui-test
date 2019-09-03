f8d20cce39c23de72c49f685f414fc24e694c821
Ian Meharg <meharg@symmetryvr.com>
2019-09-03 19:21:08 +0900

    Added worldspace 'pick up and rotate' sample.

---
+++ Assets/PickUpAndRotateButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using System;
+
+public class PickUpAndRotateButton : MonoBehaviour
+{
+    public Transform target;
+
+    private bool animating = false;
+
+    void Awake()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+        transform.DOLocalRotate(new Vector3(0f,0f,360f), 2f, RotateMode.LocalAxisAdd).SetLoops(-1);
+    }
+
+    void AnimatingFinished()
+    {
+        animating = false;
+    }
+
+    public void AnimateParent()
+    {
+        if (!animating)
+        {
+            animating = true;
+            var animationSequence = DOTween.Sequence();
+            animationSequence.Append(target.DOLocalMoveY(4f, 5f));
+            animationSequence.Insert(1f, target.DOLocalRotate(new Vector3(0, 0, 90), 2f, RotateMode.LocalAxisAdd));
+            animationSequence.OnComplete(AnimatingFinished);
+            animationSequence.Play();
+        }
+    }
+}
---
+++ Assets/PickUpAndRotateSampleMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpAndRotateSampleMain : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rotateButtonPrefab;
+
+    public Transform buttonParent;
+    public GameObject[] rotateable;
+
+    private void Start()
+    {
+        foreach (var target in rotateable)
+        {
+            var newButtonObj = GameObject.Instantiate(rotateButtonPrefab);
+            newButtonObj.transform.parent = buttonParent;
+            var newButton = newButtonObj.GetComponent<PickUpAndRotateButton>();
+            newButton.transform.localPosition = target.transform.localPosition;
+            newButton.transform.Translate(new Vector3(0, 3));
+            newButton.target = target.transform;
+        }
+    }
+
+}
